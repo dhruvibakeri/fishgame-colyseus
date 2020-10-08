ed3cd6a11b8d85271dc5e0ba8c4c53da7e8eb947
Atharva Shukla <atharvashukla282@gmail.com>
2020-10-09 02:27:14 +0530

    add data definitions and templates

--- Fish/Common/fish.js
+++ Fish/Common/fish.js
@@ -1,3 +1,52 @@
+// --------------------------------------- Data Definition ------------------------------------------------------------
+// 
+// GameState = { board: Board, next_move: PlayerColor | false, players: PlayerScores }
+// 
+// Board is Tile[]
+// 
+// PlayerScores = [PlayerColor, Score][]
+// 
+// Score is a ℕ
+// 
+// Tile = { kind: "blank_space", tile_info: TileInfo }
+//      | { kind: "hole", tile_info: TileInfo }
+//      | { kind: "fishes", tile_info: TileInfo, fishes: ℤ+ }
+//      | { kind: "player", tile_info: TileInfo, player: PlayerColor }
+// 
+// TileInfo = { size: ℕ, max_fishes: ℕ and >= 5 }
+// 
+// PlayerColor = "red" | "brown" | "black" | "white"
+//
+// --------------------------------------- Interpretation -------------------------------------------------------------
+//
+// - GameState represents the state of the game, it has the board, whose move it is next and the scores of all players.
+//
+// - Board is the board representation it is a 2 dimensional array of tiles.
+//
+// - PlayerScores is an association list of player color and their score.
+//
+// - Score is a natural number and is the number of tiles collected upto this point.
+//
+// - Tile is either a blank space (because transforming a hex grid onto a 2d array leaves some blank spaces) or a tile
+//        with a certain number of fishes on it or a tile with a player on it or (when the player eats the fish) a hole
+//        Note that a tile also keeps track of the its size and number of fishes it can accomodate.
+//
+// - TileInfo is is the size of the tile and the maximum number of fishes the tile can accomodate.
+//
+// - PlayerColor is the color of a player and is one of 4 specified colors. 
+//
+// ------------------------------------------- Templates --------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
 function add3(z) {
     return z + 3;
 }
