c5f55e93e7dc6a694fddd8ed7894001eb60ffa25
Atharva Shukla <atharvashukla282@gmail.com>
2020-10-20 21:26:46 +0530

    write html using js

--- Fish/Common/new/frontend/index.ts
+++ Fish/Common/new/frontend/index.ts
@@ -1,15 +1,49 @@
 import _ from 'lodash';
 import './style.css';
 
-import Icon, { logData } from './importData'
+import ImportedData, { logData } from './importData'
 import { currentTime } from './importData.js';
+import { fabric } from "fabric";
 
+const BANNER_WIDTH = 600;
+const BANNER_HEIGHT = 185;
 
 function main() {
   document.body.appendChild(createDiv(false));
+  addBannerAndCanvas();
 }
 
 main();
+
+function addBannerAndCanvas() {
+
+  // <br>
+  // <p class="banner">
+  //   <img src="assets/banner.jpg"
+  //        width="600px">
+  // </p>
+  // <br>
+  const bannerPara: HTMLParagraphElement = document.createElement("p");
+  const banner: HTMLImageElement = new Image(BANNER_WIDTH, BANNER_HEIGHT);
+  banner.src = ImportedData.banner;
+  bannerPara.appendChild(banner);
+  bannerPara.classList.add("banner");
+  document.body.appendChild(document.createElement("br"));
+  document.body.appendChild(bannerPara);
+  document.body.appendChild(document.createElement("br"));
+
+  // <p align="center">
+  //  <canvas id="canvas">
+  //  </canvas>
+  // </p>
+  let canvasPara: HTMLParagraphElement = document.createElement("p");
+  canvasPara.classList.add("canvasPara")
+  canvasPara.align = "center";
+  const canvasElem: HTMLCanvasElement = document.createElement("canvas");
+  canvasElem.id = "canvas";
+  canvasPara.appendChild(canvasElem);
+  document.body.appendChild(canvasPara)
+}
 
 function createDiv(fancy): HTMLDivElement {
   if (fancy) {
@@ -42,7 +76,7 @@ function addButton(divElement: HTMLDivElement): HTMLDivElement {
 function addIcon(divElement: HTMLDivElement): HTMLDivElement {
   // Add the image to our existing div.
   const myIcon = new Image();
-  myIcon.src = Icon;
+  myIcon.src = ImportedData.icon;
   divElement.appendChild(myIcon);
   return divElement;
 }
