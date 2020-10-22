76fe359ee466d4270c5d2b5755b930e364be152f
Atharva Shukla <atharvashukla282@gmail.com>
2020-10-23 01:09:21 +0530

    put contents of old in Common

--- Fish/Common/new/backend/index.spec.ts
+++
@@ -1,8 +0,0 @@
-import { assert } from 'chai';
-import { WATCH_ALL_FLAG } from './index';
-
-describe(`Testing flags`, () => {
-  it(`"watch all" flag is "--watch=backend+frontend"`, () => {
-    assert.deepEqual(WATCH_ALL_FLAG, "--watch=backend+frontend")
-  })
-})
--- Fish/Common/new/frontend/index.spec.ts
+++
@@ -1,7 +0,0 @@
-import { assert } from 'chai';
-
-describe(`one is one`, () => {
-  it(`1 = 1`, () => {
-    assert.deepEqual(1, 1)
-  })
-})
