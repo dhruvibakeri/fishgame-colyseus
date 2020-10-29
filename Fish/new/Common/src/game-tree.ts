7d76d71a0cab6921ae8a74dea310f22c3a303e6d
djb2610 <bakeri.d@northeastern.edu>
2020-10-29 23:02:11 +0530

    adding all game-tree functions

--- Fish/new/Common/src/game-tree.ts
+++ Fish/new/Common/src/game-tree.ts
@@ -1,7 +1,7 @@
 import { getReachable } from "./board-reachable";
 //------------------------------------- DATA DEFINITION -----------------------------------------------------------
 
-import { CBoard, cMove, CPenguin, CPosn, CScore, CState, getPenguinPositions, GET_currentPlayer, GET__CBoardFromCState, GET__CScoresFromCState, PRED_isCState } from "./cstate";
+import { CBoard, CMove, cMove, CPenguin, CPosn, CScore, CState, duplicateCBoard, getPenguinPositions, GET_currentPlayer, GET__CBoardFromCState, GET__CScoresFromCState, GET__CStageFromCState, PRED_isCSpaceACFish, PRED_isCState } from "./cstate";
 import { placePenguinStrategy, placingstate } from "./penguin-placement-strategy";
 import { playinginitstate } from "./run";
 
@@ -23,11 +23,11 @@ import { playinginitstate } from "./run";
 
 // An Action is one of PlayerAction | RefereeAction
 //     A PlayerAction is one of Move | PlacePenguin
-//         A Move is {kind: "move", player: UUID, posn: {from: Posn, to: Posn}}
-//         A PlacePenguin is {kind: "placePenguin", player: UUID, posn: Posn}
+//         A Move is {kind: "move", move : CMove}
+//         A PlacePenguin is {kind: "place", player: CPenguin, posn: CPosn}
 //     A RefereeAction is one of MakeHole | PlaceFish
-//         A MakeHole is {kind: "makeHole", posn: Posn}
-//         A PlaceFish is a {kind: "placeFish", posn: Posn, totalFishes: 1-5 }
+//         A MakeHole is {kind: "hole", posn: CPosn}
+//         A PlaceFish is a {kind: "placeFish", posn: CPosn, totalFishes: 1-5 }
 // INTERPRETATION. Represents the actions a player/referee can take. A player can move/place penguin. A referee can
 // place a fish, make hole. [The PlayerActions are also executed by the Referee but are "requested" by the player]
 
@@ -35,6 +35,17 @@ import { playinginitstate } from "./run";
 export type SubGameTree = [CState, () => GameTree[]]
 export  type GameTree = CState | SubGameTree;
 
+export type Action = (MoveAction | PlaceAction | HoleAction | FishAction)
+
+export type MoveAction = {kind : "move", posn : CMove}
+
+export type PlaceAction = {kind: "place", player: CPenguin, posn: CPosn}
+
+export type HoleAction = {kind: "hole", posn: CPosn}
+
+export type FishAction = {kind: "placeFish", posn: CPosn, totalFishes: number }
+
+export type IllegalAction = {kind: "illegalAction"}
 
 
 // GameTree -> GameState
@@ -72,6 +83,34 @@ export function createGameTree(gameState : CState) : GameTree {
     return [gameState, () => { return getValidSubStates(gameState) }]
 }
 
+// GameState Action -> GameState | IllegalAction
+// applies action on a given GameState only if it is legal
+export function applyAction(action : Action, gs : CState) : CState | IllegalAction{
+    if (isValidAction(action, gs)) {
+        return takeAction(action, gs);
+    }
+    else {
+        return {
+            kind: "illegalAction"
+        }
+    }
+}
+
+// applyToDirectlyReachable: GameState [GameState -> T] -> [GameState, T][]
+// applies given function to all directly reachable states
+export function applyToDirectlyReachable(gs : CState, func : (state : CState) => any) : [CState, any][] {
+    let directlyReachableStates = getValidSubStates(gs)
+
+    let res : [CState, any][]= []
+
+    directlyReachableStates.forEach(s => {
+        let currentState = getStateFromTree(s)
+        res.push([currentState, func(currentState)])
+    })
+
+    return res;
+}
+
 
 // - - - - - - - - - - - - - - - - - - - - - - 
 // - - - - - - - I N T E R N A L  - - - - - - -
@@ -105,6 +144,71 @@ export function makeAllMovesForAPenguin(penguin : CPenguin, fromPosn : CPosn, gs
     })
 
     return res;
+}
+
+// Action GameState -> boolean
+// checks if given action is valid
+export function isValidAction(action : Action, gs : CState) {
+    
+    let board = GET__CBoardFromCState(gs)
+    switch (action.kind) {
+        case "move":
+            let posn : CMove = action.posn as [CPosn, CPosn]
+            return isValidMove(posn[0], posn[1], board)
+        case "place":
+            let posn_place : CPosn = action.posn as [number, number]
+            return PRED_isCSpaceACFish(board[posn_place[0]][posn_place[1]])
+        case "hole":
+            let posn_hole : CPosn = action.posn as [number, number]
+            return PRED_isCSpaceACFish(board[posn_hole[0]][posn_hole[1]])
+        case "placeFish":
+            let posn_fish : CPosn = action.posn as [number, number]
+            return PRED_isCSpaceACFish(board[posn_fish[0]][posn_fish[1]])
+        default:
+            return false
+    }
+}
+
+// Action GameState -> GameState
+// applies action to given GameState
+export function takeAction(action : Action, gs : CState): CState {
+    let new_board : CBoard = duplicateCBoard(GET__CBoardFromCState(gs))
+    switch (action.kind) {
+        case "move":
+            return cMove(gs,action.posn)
+            break;
+        case "place":
+            new_board[action.posn[0]][action.posn[1]] = action.player
+            return [GET__CStageFromCState(gs), new_board, GET__CScoresFromCState(gs)]
+            break;
+        case "hole":
+            new_board[action.posn[0]][action.posn[1]] = "hole"
+            return [GET__CStageFromCState(gs), new_board, GET__CScoresFromCState(gs)]
+            break;
+        case "placeFish":
+            new_board[action.posn[0]][action.posn[1]] = action.totalFishes
+            return [GET__CStageFromCState(gs), new_board, GET__CScoresFromCState(gs)]
+            break;
+        default:
+            return gs
+    }
+}
+
+// checks if given move is a valid move
+function isValidMove(from : CPosn, to: CPosn, board : CBoard) {
+    let reachablePoints = getReachable(board, from)
+
+    return isInArray(reachablePoints, to)
+}
+
+// checks if given item is in given list
+function isInArray(list : any[], item : any) : boolean {
+    for(let i = 0; i < list.length; i++) {
+      if(JSON.stringify(item) === JSON.stringify(list[i])) {
+          return true
+      }
+    }
+    return false
 }
 
 
