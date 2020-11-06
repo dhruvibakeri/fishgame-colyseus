2c7418592b3c9dc7fd716672819dfe204e8fa735
Atharva Shukla <atharvashukla282@gmail.com>
2020-11-06 09:05:21 +0530

    .

--- Fish/Common/dist/states/schema-state/schema-state-data-definition.js
+++ Fish/Common/dist/states/schema-state/schema-state-data-definition.js
@@ -8,40 +8,46 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.StateSchema = exports.ScoreSchema = void 0;
 const schema_1 = require("@colyseus/schema");
-class ScoreSchema extends schema_1.Schema {
-    constructor(penguincolor, score) {
-        super();
-        this.penguincolor = penguincolor;
-        this.score = score;
+let ScoreSchema = /** @class */ (() => {
+    class ScoreSchema extends schema_1.Schema {
+        constructor(penguincolor, score) {
+            super();
+            this.penguincolor = penguincolor;
+            this.score = score;
+        }
     }
-}
-__decorate([
-    schema_1.type("string")
-], ScoreSchema.prototype, "penguincolor", void 0);
-__decorate([
-    schema_1.type("number")
-], ScoreSchema.prototype, "score", void 0);
+    __decorate([
+        schema_1.type("string")
+    ], ScoreSchema.prototype, "penguincolor", void 0);
+    __decorate([
+        schema_1.type("number")
+    ], ScoreSchema.prototype, "score", void 0);
+    return ScoreSchema;
+})();
 exports.ScoreSchema = ScoreSchema;
-class StateSchema extends schema_1.Schema {
-    constructor(gamestage, rowlen, board, players) {
-        super();
-        this.gamestage = gamestage;
-        this.rowlen = rowlen;
-        this.board = board;
-        this.players = players;
+let StateSchema = /** @class */ (() => {
+    class StateSchema extends schema_1.Schema {
+        constructor(gamestage, rowlen, board, players) {
+            super();
+            this.gamestage = gamestage;
+            this.rowlen = rowlen;
+            this.board = board;
+            this.players = players;
+        }
     }
-}
-__decorate([
-    schema_1.type("string")
-], StateSchema.prototype, "gamestage", void 0);
-__decorate([
-    schema_1.type("number")
-], StateSchema.prototype, "rowlen", void 0);
-__decorate([
-    schema_1.type(["string"])
-], StateSchema.prototype, "board", void 0);
-__decorate([
-    schema_1.type([ScoreSchema])
-], StateSchema.prototype, "players", void 0);
+    __decorate([
+        schema_1.type("string")
+    ], StateSchema.prototype, "gamestage", void 0);
+    __decorate([
+        schema_1.type("number")
+    ], StateSchema.prototype, "rowlen", void 0);
+    __decorate([
+        schema_1.type(["string"])
+    ], StateSchema.prototype, "board", void 0);
+    __decorate([
+        schema_1.type([ScoreSchema])
+    ], StateSchema.prototype, "players", void 0);
+    return StateSchema;
+})();
 exports.StateSchema = StateSchema;
 //# sourceMappingURL=schema-state-data-definition.js.map
