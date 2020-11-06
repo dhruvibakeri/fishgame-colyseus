26db37ed0f26986fcba15ac28c74078d3d5dcc78
djb2610 <bakeri.d@northeastern.edu>
2020-11-06 07:43:47 +0530

    documentation

--- Fish/Common/frontend/frontend.ts
+++ Fish/Common/frontend/frontend.ts
@@ -15,6 +15,10 @@ var host = window.document.location.host.replace(/:.*/, '');
 var client = new Colyseus.Client(location.protocol.replace("http", "ws")
   + "//" + host + (location.port ? ':' + location.port : ''));
 
+// On every state change on the server side, the updated state gets renders
+// for the observers to view
+// The players would also take actions based on their turns once the onStateChange 
+// event is triggered
 client.joinOrCreate("fish").then((room: Colyseus.Room) => {
   room.onStateChange((newState) => {
     let cState = schemaToCompact(newState)
@@ -32,23 +36,38 @@ ${JSON.stringify(cState)}
 });
 
 
-export function placementStrategy(state: GameState, player: Player): BoardPosn | IllegalAction {
+// GameState Player -> BoardPosn | IllegalAction
+// A player can use this function while deciding a placement for its penguin to place
+// it according to the penguin placement strategy
+export function placementStrategy(state: GameState): BoardPosn | IllegalAction {
   return getPenguinPlacementPosn(state)
 }
 
-export function getBestActionStrategy(state: GameState, depth: number): Move {
+// GameState Number -> [BoardPosn,BoardPosn]
+// A player can use this function while deciding a move for its penguin to move
+// it according to the best gain strategy
+export function getBestActionStrategy(state: GameState, depth: number): [BoardPosn, BoardPosn] {
   return getBestAction([state, () => { return getValidSubStatesForGameBoard(state) }], depth)
 }
 
+// GameState Player -> boolean
+// A player can use this to check if it is currently their turn 
 export function isMyTurn(gs: GameState, player: Player): boolean {
   let nextTurn: Player = GET_GameStateNextToPlace(gs)
   return player === nextTurn
 }
 
+// Colyseus.room BoardPosn -> void
+// A player can use this function to send a "place" message to the referee
+// with its desired placement positon
 export function placePenguin(room: Colyseus.Room, placementPosn: BoardPosn): void {
   room.send("place", placementPosn);
 }
 
+// Colyseus.room [BoardPosn, BoardPosn] -> void
+// A player can use this function to send a "move" message to the referee
+// with its desired move
 export function movePenguin(room: Colyseus.Room, move: [BoardPosn, BoardPosn]): void {
   room.send("move", move);
+
 }
